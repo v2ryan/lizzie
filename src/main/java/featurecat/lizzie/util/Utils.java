cd3b40f6fdaed1a29a64ecbc5043ffa76df450f0
Hiraoka <khi@users.osdn.me>
2020-11-08 19:24:55 +0900

    Show score like "W+1.2" on winrate bar (#808)

--- src/main/java/featurecat/lizzie/util/Utils.java
+++ src/main/java/featurecat/lizzie/util/Utils.java
@@ -165,6 +165,10 @@ public class Utils {
   }
 
   public static double actualScoreMean(double scoreMean) {
+    return actualScoreMean(scoreMean, Lizzie.config.kataGoScoreMeanAlwaysBlack);
+  }
+
+  private static double actualScoreMean(double scoreMean, boolean alwaysBlack) {
     double score = scoreMean;
     if (Lizzie.board.getHistory().isBlacksTurn()) {
       if (Lizzie.config.showKataGoBoardScoreMean) {
@@ -174,7 +178,7 @@ public class Utils {
       if (Lizzie.config.showKataGoBoardScoreMean) {
         score = score - Lizzie.board.getHistory().getGameInfo().getKomi();
       }
-      if (Lizzie.config.kataGoScoreMeanAlwaysBlack) {
+      if (alwaysBlack) {
         score = -score;
       }
     }
@@ -184,6 +188,19 @@ public class Utils {
   public static MoveData getBestMove() {
     List<MoveData> bestMoves = Lizzie.board.getHistory().getData().bestMoves;
     return (bestMoves.size() > 0) ? bestMoves.get(0) : null;
+  }
+
+  public static String getScoreTextWithLeadingColor() {
+    MoveData bestMove = getBestMove();
+    boolean validScore = Lizzie.leelaz.isKataGo && (bestMove != null);
+    if (!validScore) {
+      return "";
+    }
+    double blackScore = actualScoreMean(bestMove.scoreMean, true);
+    String leadingColor =
+        Lizzie.frame.resourceBundle.getString(
+            (blackScore >= 0) ? "CountDialog.bigBlack" : "CountDialog.bigWhite");
+    return leadingColor + "+" + String.format("%.1f", Math.abs(blackScore));
   }
 
   public static Integer txtFieldValue(JTextField txt) {
